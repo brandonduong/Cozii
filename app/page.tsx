bed24599949439a49c98158763e7d30f45637beb
Brandon Duong <brandon.duong@outlook.com>
2023-12-05 08:07:09 -0500

    added icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import Stages from "@/components/Stages";
 import { FormProvider } from "@/contexts/FormContext";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center p-2 sm:px-24 md:px-32 lg:items-center bg-gradient-to-t from-black to-red-700 gap-2">
-      <span className="text-center mb-6">
+    <main className="flex min-h-screen flex-col justify-center p-2 items-center bg-gradient-to-t from-black to-red-700 gap-2">
+      <span className="text-center">
         <h1 className="text-4xl font-black">Two-way Trip to Mars</h1>
         <h6 className="text-xs">We try to bring you back!</h6>
       </span>
+      <div className="bg-white rounded-full p-2 border-4 border-black">
+        <Image src="/mars.png" width={128} height={128} alt="mars" />
+      </div>
+
       <FormProvider>
         <Stages />
       </FormProvider>
